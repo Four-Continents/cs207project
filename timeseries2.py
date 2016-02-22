5d37681e92ed11b0105d6e5fd34de878df62d1f0
Amy <amy.mae.lee@gmail.com>
2016-02-22 04:13:43 -0500

    Resolve merge conflicts

--- timeseries2.py
+++ timeseries2.py
@@ -4,6 +4,12 @@ from doctest import run_docstring_examples as dtest
 import numbers
 import lazy as lz
 
+# TODO should be monotonically increasing (you don't need to enforce this right now
+# TODO Victor's comment about binary search
+# Nice work! Since the times must be monotonically increasing, we can also do a binary search to get the O(lg(N))
+# lookup. Should also refactor position = np.where(self._times == index)[0] out to a separate function if we do this.
+
+
 class TimeSeries(object):
     """
     A class which stores a single ordered sequence of numerical data, and supports lookup and modification operations
@@ -25,7 +31,7 @@ class TimeSeries(object):
     TypeError: iterable must contain numerical types
     >>> a = TimeSeries(range(0,1000000))
     >>> a
-    [0, 1, 2, 3, 4, 5, ...]
+    [0, 1, 2, 3, 4, 5, ..g.]
     >>> print(a)
     [0, 1, 2, 3, 4, 5, ...]
     >>> len(a)
@@ -50,11 +56,10 @@ class TimeSeries(object):
         TypeError
             Raises the error if the input data is not a sequence or if it contains any type that is not numerical
         """
+        def convert_to_np_array(data):
 
-        def CheckNumpy(data):
-
-            if not hasattr(data, '__len__') and not hasattr(data, '__getitem__') and not hasattr(self._data, '__array_interface__') and not \
-                hasattr(self._data, '__array__'):
+            if not hasattr(data, '__len__') and not hasattr(data, '__getitem__') and not \
+                    hasattr(data, '__array_interface__') and not hasattr(data, '__array__'):
                 msg = 'Passed in parameter must be an array, any object exposing the array interface, an object whose ' \
                   '__array__ method returns an array, or any (nested) sequence.'
                 raise TypeError(msg)
@@ -66,9 +71,8 @@ class TimeSeries(object):
 
             return np.array(data)
 
-
-        self._times = CheckNumpy(times)
-        self._values = CheckNumpy(values)
+        self._times = convert_to_np_array(times)
+        self._values = convert_to_np_array(values)
 
     def __iter__(self):
         """
@@ -81,8 +85,6 @@ class TimeSeries(object):
         for x in self._values:
             yield x
 
-
-
     def __len__(self):
         """
         Returns the length of the iterable or sequence
@@ -91,9 +93,10 @@ class TimeSeries(object):
         -------
         int
         """
+        # TODO check this works
+        assert len(self._times) == len(self._values)
+
         return len(self._times)
-
-
 
     def __getitem__(self, index):
         """
@@ -117,8 +120,6 @@ class TimeSeries(object):
         else:
             return self._values[position]
 
-
-
     def __setitem__(self, index, value):
         """
         Setter for the class - used to modify individual values in the TimeSeries given a particular time point
@@ -138,7 +139,6 @@ class TimeSeries(object):
         else:
             self._values[position] = value
 
-
     def __contains__(self, index):
 
         position = np.where(self._times == index)[0]
@@ -148,37 +148,63 @@ class TimeSeries(object):
         else:
             return True
 
+    def binary_search(self, value):
+        pass
 
-#     def __repr__(self):
-#         """
-#         Returns
-#         -------
-#         string
-#             A string representation of the sequence data. Truncates longer sequences using the reprlib library.
-#         """
-#         return reprlib.repr(self._data)
+    def __repr__(self):
+        """
+        Returns
+        -------
+        string
+            A string representation of the sequence time series values. Truncates longer sequences using the reprlib library.
+        """
+        return reprlib.repr(self._values)
 
     def __str__(self):
         """
         Returns
         -------
         string
-            A string representation of the sequence data. Truncates longer sequences using the reprlib library.
+            A string representation of the sequence of time series values. Truncates longer sequences using the reprlib library.
         """
         return str("TimeSeries" + reprlib.repr(list(zip(self._times, self._values))))
 
     def values(self):
-
+        """
+        Returns
+        -------
+        sequence
+            a sequence of the time series values
+        """
         return self._values
 
     def times(self):
+        """
+        Returns
+        -------
+        sequence
+            a sequence of the time series times
+        """
 
-        return reprlib.repr(self._times)
+        return self._times
 
     def items(self):
+        """
+        Returns
+        -------
+        sequence
+            a sequence of of (time, value) tuples
+        """
+        return zip(self._times, self._values)
 
-        return zip(times, values)
+    def interpolate(self):
+        pass
 
     @property
     def lazy(self):
         return lz.lazy(lambda x: x)(self)
+
+
+if __name__ == '__main__':
+    dtest(TimeSeries, globals(), verbose = True)
+
