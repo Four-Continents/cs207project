b9e6ab600e0436cb8b7997b12be39ae94d443a5c
Fanny Heneine <fannyheneine@g.harvard.edu>
2016-03-27 16:35:27 -0400

    PyPe Part 2 completed with bug fixes

--- pype/pype/semantic_analysis.py
+++ pype/pype/semantic_analysis.py
@@ -14,13 +14,13 @@ class CheckSingleAssignment(ASTVisitor):
     self._curr_component = None
   def visit(self, node):
     if isinstance(node, ASTComponent):
-     	self._curr_component = node.name.name
+        self._curr_component = node.name.name
         self._component_frames[node.name.name] = set()
     elif isinstance(node, ASTAssignmentExpr):
-    	if node.binding.name in self._component_frames[self._curr_component]:
-        	raise ValueError("Trying to assign a variable twice: " + node.binding.name)
+        if node.binding.name in self._component_frames[self._curr_component]:
+            raise ValueError("Trying to assign a variable twice: " + node.binding.name)
         else:
-        	self._component_frames[self._curr_component].add(node.binding.name)
+            self._component_frames[self._curr_component].add(node.binding.name)
 
 class CheckSingleIOExpression(ASTVisitor):
   def __init__(self):
