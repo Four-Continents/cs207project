0275c58e20e5d760f34d9e112bf7acf953f40c2c
Fanny Heneine <fannyheneine@g.harvard.edu>
2016-03-27 16:23:51 -0400

    Pype Part 2 Done and Corrected

--- pype/pype/semantic_analysis.py
+++ pype/pype/semantic_analysis.py
@@ -10,15 +10,15 @@ class PrettyPrint(ASTVisitor):
 class CheckSingleAssignment(ASTVisitor):
   def __init__(self):
     super().__init__()
-        self._component_frames = {}
-        self._curr_component = None
+    self._component_frames = {}
+    self._curr_component = None
   def visit(self, node):
     if isinstance(node, ASTComponent):
-            self._curr_component = node.name.name
-            self._component_frames[node.name.name] = set()
+     	self._curr_component = node.name.name
+        self._component_frames[node.name.name] = set()
         elif isinstance(node, ASTAssignmentExpr):
-            if node.binding.name in self._component_frames[self._curr_component]:
-                raise ValueError("Trying to assign a variable twice: " + node.binding.name)
+        	if node.binding.name in self._component_frames[self._curr_component]:
+            	raise ValueError("Trying to assign a variable twice: " + node.binding.name)
             else:
                 self._component_frames[self._curr_component].add(node.binding.name)
 
