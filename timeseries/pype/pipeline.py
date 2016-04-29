6469f845b9fe6ee739106c7e5935f06f158afa75
Amy <amy.mae.lee@gmail.com>
2016-04-28 23:33:36 -0400

    resolve conflicts, update pipeline.py

--- timeseries/pype/pipeline.py
+++ timeseries/pype/pipeline.py
@@ -4,11 +4,13 @@ from .ast import *
 from .semantic_analysis import CheckSingleAssignment, CheckSingleIOExpression, CheckUndefinedVariables
 from .translate import SymbolTableVisitor, LoweringVisitor
 from .optimize import *
+from .pcode import PCodeGenerator
 
 class Pipeline(object):
   def __init__(self, source):
-    with open(source) as f:
-      self.compile(f)
+      self.pcodes = {}
+      with open(source) as f:
+        self.compile(f)
 
   def compile(self, file):
     input = file.read()
@@ -28,5 +30,14 @@ class Pipeline(object):
     # Optimization
     ir.flowgraph_pass( AssignmentEllision() )
     ir.flowgraph_pass( DeadCodeElimination() )
+    ir.topological_flowgraph_pass( InlineComponents() )
+
+    # PCode Generation
+    pcodegen = PCodeGenerator()
+    ir.flowgraph_pass( pcodegen )
+    self.pcodes = pcodegen.pcodes
 
     return ir
+
+  def __getitem__(self, component_name):
+    return self.pcodes[component_name]
