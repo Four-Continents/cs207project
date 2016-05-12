fdc3ffb96108da68faed8c281c25444857bc443d
vinayps <vinayps89@gmail.com>
2016-05-12 11:11:15 -0400

    sync branch

--- timeseries/test.py
+++ timeseries/test.py
@@ -18,17 +18,33 @@ schema = {
   'std': {'convert': float, 'index': 1},
   'vp': {'convert': bool, 'index': 1}
 }
-# client = TSDBClient(30000)
-# client.insert_ts("fifty", ts.TimeSeries([1, 2, 3], [2, 4, 9]))
-# client.upsert_meta('fifty', {'order': 1, 'blarg': 1})
-# client.select({'order': 1}, fields=['order', 'ts'], additional={'sort_by': '-order'})
-# print("done")
+client = TSDBClient(30000)
+client.insert_ts("fifty", ts.TimeSeries([1, 2, 3], [2, 4, 9]))
+client.upsert_meta('fifty', {'order': 1, 'blarg': 1})
+
+client.insert_ts("sixty", ts.TimeSeries([1, 2, 3], [2, 4, 9]))
+client.upsert_meta('sixty', {'order': 2, 'blarg': 1})
+
+client.insert_ts("seventy", ts.TimeSeries([1, 2, 3], [2, 4, 9]))
+client.upsert_meta('seventy', {'order': 3, 'blarg': 1})
+
+# res = client.select({'order':{'>':float('-inf')}}, fields=['order', 'ts'])
+res = client.select(fields=['order', 'ts'])
+print(res)
+
+res = client.delete('fifty')
+print(res)
+
+res = client.select(fields=['order', 'ts'], additional={'sort_by':'+order'})
+print (res)
+
 # db = connect("/tmp/test2.dbdb", "/tmp/test2_idx.dbdb", schema)
 #
-# db.insert_ts("two", ts.TimeSeries([1, 2, 3], [2, 4, 9]).to_json())
+# db.insert_ts("two", ts.TimeSeries([1, 2, 3], [2, 4, 9]))
 # print(db.select({'order': -1}, fields=['order', 'ts'], additional={'sort_by': '-order'}))
-# print(db.get("three"))
-# time.sleep(500)
+# # # print(db.get("three"))
+# # time.sleep(500)
+# print(db.select({'order': -1}, fields=['order', 'ts'], additional={''}))
 # db.close()
 
 # print("Test one")
@@ -44,7 +60,7 @@ schema = {
 #     print(db.get("rahul"))
 # except:
 #     print("failed like it was supposed to")
-#
+
 # print("Test two")
 # db.set("rahul", "aged")
 # db.set("pavlos", "aged")
@@ -53,7 +69,7 @@ schema = {
 # db.close()
 # db = connect("/tmp/test2.dbdb")
 # print(db.get("rahul"))
-#
+
 # print("Test three")
 # db.set("rahul", "young")
 # print(db.get("rahul"))
